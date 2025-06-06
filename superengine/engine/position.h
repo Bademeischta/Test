d8e6058b5ff27f8ba305ed5866ac68bd8301f7fa
Bademeischta <148655249+Bademeischta@users.noreply.github.com>
2025-06-06 08:17:30 +0200

    Add roadmap checklist

--- superengine/engine/position.h
+++ superengine/engine/position.h
@@ -10,6 +10,10 @@ struct Position {
     Bitboard occupied_bb[2]{};
     Bitboard all_occupied{};
     Color side{};
+    uint8_t castling_rights{}; // bit0=K, bit1=Q, bit2=k, bit3=q
+    int8_t en_passant{-1};    // -1 if none
+    int halfmove_clock{};
+    int fullmove_number{1};
 
     Position() = default;
     explicit Position(const std::string& fen);
@@ -17,4 +21,5 @@ struct Position {
     Bitboard pieces(Piece pc, Color c) const { return piece_bb[c][pc]; }
     Bitboard occupied() const { return all_occupied; }
     Color side_to_move() const { return side; }
+    Piece piece_on(int sq) const;
 };
