efc964de03aa464c419ae8989fbf4445e736e2b5
Bademeischta <148655249+Bademeischta@users.noreply.github.com>
2025-06-12 14:22:03 +0200

    docs: add ONNX Runtime setup

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,7 +14,12 @@ if [ -d "superengine" ]; then
   cd superengine
   mkdir -p build
   cd build
-  cmake ..
+  if [ -n "$ONNXRuntime_DIR" ]; then
+    echo "Using ONNXRuntime from $ONNXRuntime_DIR"
+    cmake .. -DONNXRuntime_DIR="$ONNXRuntime_DIR"
+  else
+    cmake ..
+  fi
 
   # Determine the number of parallel build jobs in a cross-platform way
   JOBS=1
@@ -24,11 +29,6 @@ if [ -d "superengine" ]; then
     JOBS=$(sysctl -n hw.ncpu)
   fi
   cmake --build . --parallel "$JOBS"
-
-
-  cmake --build . -- -j$(nproc)
-
-  make -j$(nproc)
 
 
   cd ../..
